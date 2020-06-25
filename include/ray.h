739af68eb286103470ba834b832ba11009523561
Yakov Gazizov <imcuber@live.ru>
2020-06-25 23:09:21 +0500

    Implement ray_t

--- include/ray.h
+++ include/ray.h
@@ -1,4 +1,25 @@
 #ifndef GRAPHICS_COMMON_MATH_RAY_H__
 #define GRAPHICS_COMMON_MATH_RAY_H__
 
+namespace graphics{ namespace math{
+
+template <typename RealType>
+union vec3_t;
+
+template <typename RealType>
+union norm3_t;
+
+template <typename RealType>
+struct ray_t
+{
+    vec3_t<RealType> origin;
+    norm3_t<RealType> direction;
+
+    ray_t(const vec3_t<RealType>& origin, const norm3_t<RealType>& direction) noexcept;
+    static ray_t fromTwoPoints(const vec3_t<RealType> &from, const vec3_t<RealType> &to) noexcept;
+};
+
+} // namespace math
+} // namespace graphics
+
 #endif // GRAPHICS_COMMON_MATH_RAY_H__
--- source/ray.cpp
+++ source/ray.cpp
@@ -1,3 +1,24 @@
 #include "ray.h"
 
-#include <cmath>
+#include "vec.h"
+#include "norm.h"
+
+namespace graphics::math{
+
+template <typename RealType>
+ray_t<RealType>::ray_t(const vec3_t<RealType> &origin, const norm3_t<RealType> &direction) noexcept:
+    origin{ origin },
+    direction{ direction }
+{}
+template ray_t<float>::ray_t(const vec3_t<float> &origin, const norm3_t<float> &direction) noexcept;
+template ray_t<double>::ray_t(const vec3_t<double> &origin, const norm3_t<double> &direction) noexcept;
+
+template <typename RealType>
+ray_t<RealType> ray_t<RealType>::fromTwoPoints(const vec3_t<RealType> &from, const vec3_t<RealType> &to) noexcept
+{
+    return ray_t(from, to - from);
+}
+template ray_t<float> ray_t<float>::fromTwoPoints(const vec3_t<float> &from, const vec3_t<float> &to) noexcept;
+template ray_t<double> ray_t<double>::fromTwoPoints(const vec3_t<double> &from, const vec3_t<double> &to) noexcept;
+
+} // namespace graphics::math
