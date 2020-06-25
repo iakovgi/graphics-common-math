f14eef150dd4f73a7be1b5818fbc21b33f6434ef
Yakov Gazizov <imcuber@live.ru>
2020-06-25 22:50:22 +0500

    Implement Random

--- include/random.h
+++ include/random.h
@@ -1,4 +1,24 @@
 #ifndef GRAPHICS_COMMON_MATH_RANDOM_HPP__
 #define GRAPHICS_COMMON_MATH_RANDOM_HPP__
 
+#include <random>
+
+class Random
+{
+public:
+    Random(std::uint32_t seed = 0);
+    ~Random() = default;
+
+    Random(const Random& that) = delete;
+    Random(Random&& that) = delete;
+    Random& operator=(const Random& that) = delete;
+    Random& operator=(Random&& that) = delete;
+
+    double getUniform() noexcept;
+
+private:
+    std::default_random_engine m_engine;
+    std::uniform_real_distribution<double> m_distribution;
+};
+
 #endif // GRAPHICS_COMMON_MATH_RANDOM_HPP__
--- source/random.cpp
+++ source/random.cpp
@@ -1,3 +1,22 @@
 #include "random.h"
 
 #include <random>
+#include <chrono>
+
+Random::Random(std::uint32_t seed):
+    m_engine{},
+    m_distribution{}
+{
+    if(seed == 0) {
+        const auto now = std::chrono::system_clock::now();
+        const auto sinceEpoch = now.time_since_epoch();
+        const auto sinceEpochDuration = std::chrono::duration_cast<std::chrono::milliseconds>(sinceEpoch);
+        seed = (std::uint32_t)sinceEpochDuration.count();
+    }
+    m_engine.seed(seed);
+}
+
+double Random::getUniform() noexcept
+{
+    return m_distribution(m_engine);
+}
